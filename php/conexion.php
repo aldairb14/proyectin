1ed7d9b844adbf6bd227153a6ab3801a012d138c
aldairb14 <34193769+aldairb14@users.noreply.github.com>
2017-12-08 14:41:30 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -16,11 +16,7 @@ var $host		=	"localhost";
 var $username	=	"root";
 var $passUser	=	"";
 var $database	=	"secundaria";
-//var $host		=	"www.secundariatec16.000webhostapp.com";
-//var $username	=	"id3866439_root";
-//var $passUser	=	"laison";
-//var $database	=	"id3866439_secundaria";
-//$conexion1=mysqli_connect($host, $username, $passUser, $database);
+
 var $connection;
 
 public function conectar(){
